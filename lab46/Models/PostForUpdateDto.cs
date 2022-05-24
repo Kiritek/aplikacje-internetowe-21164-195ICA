cabd9f469a75870658f878f1257aff943437ef2f
Piotr Ożdziński <84562925+Kiritek@users.noreply.github.com>
2022-05-24 17:18:21 +0200

    Delete lab46 directory

--- lab46/Models/PostForUpdateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RestApiAppv2.Models
-{
-    public class PostForUpdateDto
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
-    }
-}
